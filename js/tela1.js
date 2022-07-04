254ad58106f9e7ce05f831fbf9b0935c858b79e2
voller <voller.silas@gmail.com>
2022-07-04 09:45:40 -0300

    Trocando para tela 3 ao clicar em criar quiz

--- js/tela1.js
+++ js/tela1.js
@@ -9,48 +9,42 @@ function comparador() {
 
 function iniciaPagina() {
   window.scrollTo(0, 0);
-  buscarQuizzes();
 }
 
-
 function mostrarTela3() {
-  const upFront = document.querySelector(".upfront");
-  upFront.classList.add("hide");
-  const tela3 = document.querySelector(".tela3");
-  tela3.classList.remove("hide");
-  console.log(tela3);
-  console.log(upFront);
+  document.querySelector(".upfront").classList.add("hide");
+  document.querySelector(".tela3").classList.remove("hide");
 }
 
 iniciaPagina();
 
 function SearchAllQuizezz() {
-        console.log('rodou')
-        let quizzes = axios.get('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes')
-        quizzes.then(ShowQuizzes)
-        quizzes.catch(error)
-        console.log(quizzes)
-        alert()
+  console.log("rodou");
+  let quizzes = axios.get(
+    "https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes"
+  );
+  quizzes.then(ShowQuizzes);
+  quizzes.catch(error);
+  console.log(quizzes);
+  alert();
 }
 
-function ShowQuizzes(elemento){
-    const allquizz = elemento.data;
-    const divquizz = document.querySelector(".quiz-list")
-    divquizz = ""
-    console.log(allquizz)
-    allquizz.forEach(function(number) {
-        console.log(number)
-        divquizz.innerHTML += `<div class="quizz">
+function ShowQuizzes(elemento) {
+  const allquizz = elemento.data;
+  const divquizz = document.querySelector(".quiz-list");
+  divquizz = "";
+  console.log(allquizz);
+  allquizz.forEach(function (number) {
+    console.log(number);
+    divquizz.innerHTML += `<div class="quizz">
         <img src="${number.image}">
         <div class="overlay"></div>
         <div class="titulo1">${number.title}</div>
-      </div>`
-
-    });
-    
+      </div>`;
+  });
 }
 
-function error(){
-    alert('Deu erro')
+function error() {
+  alert("Deu erro");
 }
-SearchAllQuizezz()
+SearchAllQuizezz();
