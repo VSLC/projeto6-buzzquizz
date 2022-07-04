3b2b283ffbf3e16e5083c07f4bb234c535e59322
Ana Flávia Estacheski <flaviaestacheski@gmail.com>
2022-07-04 11:00:17 -0300

    merge commits

--- js/tela1.js
+++ js/tela1.js
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
+
+
+
+
 let acerto = 0;
 let idQuiz;
 let quiz;
@@ -24,45 +27,3 @@ function mostrarTela3() {
 }
 
 iniciaPagina();
-=======
-
-function mostrarTela3() {
-    const upFront = document.querySelector(".upfront");
-    upFront.classList.add("hide");
-    const tela3 = document.querySelector(".tela3");
-    tela3.classList.remove("hide");
-    console.log(tela3);
-    console.log(upFront);
-}
-
-function SearchAllQuizezz() {
-        console.log('rodou')
-        let quizzes = axios.get('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes')
-        quizzes.then(ShowQuizzes)
-        quizzes.catch(error)
-        console.log(quizzes)
-        alert()
-}
-
-function ShowQuizzes(elemento){
-    const allquizz = elemento.data;
-    const divquizz = document.querySelector(".quiz-list")
-    divquizz = ""
-    console.log(allquizz)
-    allquizz.forEach(function(number) {
-        console.log(number)
-        divquizz.innerHTML += `<div class="quizz">
-        <img src="${number.image}">
-        <div class="overlay"></div>
-        <div class="titulo1">${number.title}</div>
-      </div>`
-
-    });
-    
-}
-
-function error(){
-    alert('Deu erro')
-}
-SearchAllQuizezz()
->>>>>>> 05b67ec69b495006c05d1296ab5413b68fe3f289
